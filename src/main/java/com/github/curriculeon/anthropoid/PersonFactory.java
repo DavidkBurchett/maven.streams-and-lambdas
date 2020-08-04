7d32352d83e4f213a163fd7d9b3e3eba7a3f8743
David_Kyle_Burchett <davidkburchett@gmail.com>
2020-08-04 11:05:28 -0400

    finished PersonFactory

--- src/main/java/com/github/curriculeon/anthropoid/PersonFactory.java
+++ src/main/java/com/github/curriculeon/anthropoid/PersonFactory.java
@@ -3,8 +3,12 @@ package com.github.curriculeon.anthropoid;
 import com.github.curriculeon.tools.RandomUtils;
 import com.github.curriculeon.tools.StringUtils;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -38,7 +42,9 @@ public final class PersonFactory {
      * @return - ArrayList of Person objects
      */ // TODO
     public List<Person> createPersonList(int listSize) {
-        return null;
+        return Stream.generate(this::createRandomPerson)
+                        .limit(listSize)
+                        .collect(Collectors.toList());
     }
 
 
@@ -47,7 +53,11 @@ public final class PersonFactory {
      * @return - Array of Person objects
      */ // TODO
     public Person[] createPersonArray(int arrayLength) {
-        return null;
+        Person[] personArray = new Person[arrayLength];
+        Stream.generate(this::createRandomPerson)
+                .limit(arrayLength)
+                .toArray(value -> personArray);
+        return personArray;
     }
 
 
@@ -58,6 +68,7 @@ public final class PersonFactory {
      * @return - Stream representation of collection of Person objects
      */ // TODO
     public Stream<Person> createPersonStream(int streamCount) {
-        return null;
+        return Stream.generate(this::createRandomPerson)
+                .limit(streamCount);
     }
 }
